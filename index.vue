de934402409c5823f14d7652ac8e1f4e2eadf5bb
littleOneYuan <53067129+littleOneYuan@users.noreply.github.com>
2021-03-15 18:28:00 +0800

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="c-combine">
     <div class="comBox">
-      <div v-for="(item,idx) in com_arr" :key="item.id" class="comcell" :class="{'selcomcell': item.selected}" @click="selcom_handle()">
+      <div v-for="(item,idx) in com_arr" :key="item.id" class="comcell" :class="{'selcomcell': item.selected}" @click="selcom_handle(idx)">
         <span v-if="!item.edit" @dblclick.stop="name_click(2, idx)" @click.stop="name_click(1, idx)">{{item.name}}</span>
-        <input v-else v-model="item.name" type="text" placeholder="组合名" maxlength="5" class="name-input" @focus="name_input(idx)" @blur="input_over(idx)">
+        <input v-else v-model="item.name" type="text" placeholder="组合名" maxlength="5" class="name-input" @focus="name_input(idx)" @blur="input_over(idx,item.name)" @click.stop>
         <Icon type="md-close-circle" @click.stop="close_com(idx)" />
       </div>
       <button type="text" class="add-btn" @click="addcom_handle()">+添加</button>
@@ -11,82 +11,119 @@
   </div>
 </template>
 <script>
-import { genID } from '@/common/js/c_common'
+import { genID, isArray } from './com.js'
 export default {
   name: 'c-combine',
   data () {
     return {
-      cell_status: [1, 0],
-      com_arr: [{
-        name: '组合1',
-        content: '',
-        id: 0,
-        selected: true,
-        edit: false
-      }, {
-        name: '组合2',
-        content: '',
-        id: 1,
-        selected: false,
-        edit: false
-      }]
+      cell_status: [1, 0]
     }
   },
   computed: {
   },
   props: {
-
+    com_arr: {
+      type: Array,
+      default () {
+        return [{ // 默认组合
+          name: '组合1',
+          content: {},
+          id: 0,
+          selected: true,
+          edit: false
+        }, {
+          name: '组合2',
+          content: {},
+          id: 1,
+          selected: false,
+          edit: false
+        }]
+      }
+    }
   },
   methods: {
     // 输入组合名字
     name_input (idx) {
-
+      // console.log('聚焦')
     },
     // 组合名字输入完毕
-    input_over (idx) {
+    input_over (idx, name) {
       this.com_arr[idx].edit = false
-      console.log('失焦')
+      // console.log('失焦')
+      console.log('子：' + name)
+      this.$emit('changename', idx, name)
+      this.$emit('synfun', this.com_arr)
     },
     name_click (_type, idx) {
+      var self = this
       clearTimeout(this.timer)
-      if (_type === 1) {
+      if (_type === 1) { // 单击某个组合
         if (event.detail === 2) return
         this.timer = setTimeout(function () {
-          console.log(genID(20))
-          console.log('单击')
-        }, 300)
+          // console.log('单击')
+          let last_sel_id = 0
+          if (isArray(self.com_arr)) {
+            self.com_arr.forEach(com => {
+              if (com.selected)last_sel_id = com.id
+              com.edit = false
+              com.selected = false
+            })
+            self.com_arr[idx].selected = true
+            self.$emit('fixfun', 'click', last_sel_id, idx)
+          }
+        }, 100)
       } else {
-        console.log('双击')
+        // console.log('双击')
         this.com_arr[idx].edit = true
       }
+      this.$emit('synfun', this.com_arr)
     },
-    selcom_handle (e) {
-      console.log(e)
-      alert('成功')
+    // 选中某个组合
+    selcom_handle (idx) {
+      if (isArray(this.com_arr)) {
+        let last_sel_id = 0
+        this.com_arr.forEach(com => {
+          if (com.selected)last_sel_id = com.id
+          com.selected = false
+        })
+        this.com_arr[idx].selected = true
+        this.$emit('fixfun', 'click', last_sel_id, idx)
+        this.$emit('synfun', this.com_arr)
+      }
     },
+    // 删除某个组合
     close_com (idx) {
       alert('删除' + (idx + 1) + 'id: ' + this.com_arr[idx].id)
       if (this.com_arr[idx].selected) {
         this.com_arr.splice(idx, 1)
-        this.com_arr[this.com_arr.length - 1].selected = true
+        this.com_arr[0].selected = true
       } else {
         this.com_arr.splice(idx, 1)
       }
+      this.$emit('delfun', idx)
+      this.$emit('synfun', this.com_arr)
     },
     // 新增组合
     addcom_handle () {
-      this.com_arr.forEach(com => {
-        com.selected = false
-      })
-      // const name_temp = '组合' + (this.com_arr.length + 1)
-      const id_temp = this.com_arr.length
-      this.com_arr.push({
-        name: '',
-        content: '',
-        id: id_temp,
-        selected: true,
-        edit: true
-      })
+      if (isArray(this.com_arr)) {
+        let last_sel_id = 0
+        this.com_arr.forEach(com => {
+          if (com.selected)last_sel_id = com.id
+          com.selected = false
+          com.edit = false
+        })
+        const id_temp = genID(1)
+        const addcom = {
+          name: '双击命名',
+          content: {},
+          id: id_temp,
+          selected: true,
+          edit: true
+        }
+        this.com_arr.push(addcom)
+        this.$emit('fixfun', 'add', last_sel_id, addcom)
+        this.$emit('synfun', this.com_arr)
+      }
     }
   },
   watch: {
@@ -98,74 +135,5 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-.comBox {
-  padding: 6px 8px;
-  background-color: #a197e059;
-  border-radius: 6px;
-  border: 1px solid rgba(93,73,204,0.741);
-  box-shadow: 0px 1px 2px 0px #3c3086cf;
-  &:focus {
-    border: 2px solid rgba(93,73,204,0.741)
-  }
-}
-.comcell, .selcomcell {
-  display: inline-block;
-  margin-right: 10px;
-  background-color: #fff;
-  border-radius: 5px;
-  border: 1px solid #8476d7;
-  width: 80px;
-  height: 26px;
-  span, i {
-    display: inline-block;
-    font-size: 14px;
-    text-align: center;
-    line-height: 24px;
-    color: #8476d7
-    &:hover {
-      cursor: pointer
-    }
-  }
-  span {
-    font-weight: 600;
-    width: 60px;
-    &:hover {
-      background-color #e7e2ff
-    }
-  }
-  i {
-    flex: 1
-    &:hover {
-      font-size: 15px;
-    }
-  }
-}
-.selcomcell {
-  background-color: #8476d7;
-  span, i {
-    color: #fff
-  }
-  span {
-    &:hover {
-      background-color: #8476d7;
-    }
-  }
-  input.name-input {
-    background: #8476d7;
-  }
-}
-input.name-input {
-  width: 50px;
-  background: #fff;
-  margin-left: 10px;
-  line-height: 24px;
-  font-size: 14px;
-  color: #fff;
-}
-button.add-btn {
-  padding: 2px 4px;
-  color: #8073d6;
-  font-weight: 600;
-  background: #fff0;
-}
+@import './com.styl';
 </style>
